5ee5d0e42b20376cd3782f8dee2b5f58737f32a6
Yannic Luyckx <yannic.luyckx@gmail.com>
2018-04-12 23:04:10 -0400

    add slack receiver

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-var messages {'a':'b'};
+var messages = [];
 
 app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
@@ -13,3 +13,22 @@ app.use(express.static('public'));
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+
+const { RTMClient } = require('@slack/client');
+
+const token = process.env.SLACK_TOKEN;
+
+// The client is initialized and then started to get an active connection to the platform
+const rtm = new RTMClient(token);
+rtm.start();
+
+
+rtm.on('message', (event) => {
+  // For structure of `event`, see https://api.slack.com/events/message
+  var message = event;
+  console.log('message: %j', message);
+
+  messages.push(message);
+
+});
